d46c7979dfc5a31da11cf1538f37eaa85efe172d
Sudo-gits <90916613+Sudo-gits@users.noreply.github.com>
2023-05-30 23:09:26 +0530

    Add files via upload

---
+++ ytclone/script.js
@@ -0,0 +1,10 @@
+var menu=document.querySelector(".menu-icon");
+var sidebar=document.querySelector(".left");
+
+menu.onclick = function(){
+sidebar.classList.toggle("shrink");
+}
+
+// menu.addEventListener(onclick=>{
+
+// })
